6e66999d6126b1f4a01d7886caeabc19b64ce214
cgaeta <datopher64@gmail.com>
2018-02-17 18:27:27 -0500

    Update Hex.js

--- src/components/Hex.js
+++ src/components/Hex.js
@@ -7,6 +7,7 @@ const Hex = (props) => {
 
   let {entry} = props;
   let {data} = entry;
+  let src = "", title = "Title", text = "text", n = 2;
   //let {src, title, text, n} = data;
   //let {src = "", title = "Title", text = "Text"} = data,
   //    n = 2;
