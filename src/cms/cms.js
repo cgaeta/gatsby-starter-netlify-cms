164f88a273e2398f512e7b6a803327e5a30cb186
cgaeta <datopher64@gmail.com>
2018-02-17 14:23:02 -0500

    Update cms.js

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -4,8 +4,10 @@ import 'netlify-cms/dist/cms.css';
 import AboutPagePreview from './preview-templates/AboutPagePreview';
 import BlogPostPreview from './preview-templates/BlogPostPreview';
 import ProductPagePreview from './preview-templates/ProductPagePreview';
+import Hex from './components/Hex.js';
 
 CMS.registerPreviewStyle('/styles.css');
 CMS.registerPreviewTemplate('about', AboutPagePreview);
 CMS.registerPreviewTemplate('products', ProductPagePreview);
 CMS.registerPreviewTemplate('blog', BlogPostPreview);
+CMS.registerPreviewTemplate('hex', Hex);
