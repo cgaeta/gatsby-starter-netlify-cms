4f51a4897db5b93a49cb6a7e42671e0df10593f3
cgaeta <datopher64@gmail.com>
2018-02-17 18:50:41 -0500

    Create HexPreview.js

---
+++ src/cms/preview-templates/HexPreview.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Hex } from '../../components/Hex.js';
+
+const HexPreview = ({ entry, widgetFor }) => (
+  <Hex
+    src={entry.getIn(['data', 'src'])}
+    text={entry.getIn(['data', 'text'])}
+    title={entry.getIn(['data', 'title'])}
+    n={entry.getIn(['data', 'n'])}
+  />
+);
+
+export default HexPreview;
