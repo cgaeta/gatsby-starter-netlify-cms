5fcfe2e2d0ccdafd9767062baf1062b60a4b07a6
cgaeta <datopher64@gmail.com>
2018-02-17 14:17:01 -0500

    Add files via upload

---
+++ src/components/index.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+const sixtyDeg = Math.PI / 3;
+
+const Hex = ({src, title, text, n}) => {
+
+  return (
+  <div className="hex" style={{
+    position: "relative",
+    visibility: "hidden",
+    outline: "1px solid transparent",
+    width: `calc(100% / ${n})`
+  }}>
+    <div className="hexIn" style={{
+      position: "absolute",
+      width: "96%",
+      paddingBottom: `calc(96% / ${Math.sin(sixtyDeg)})`,
+      margin: "0 2%",
+      overflow: "hidden",
+      visibility: "hidden",
+      transform: "rotate3d(0,0,1, -60deg) skewY(30deg)",
+      outline: "1px solid transparent"
+    }}>
+      <div className="hexLink">
+        <img src={src} alt />
+        <h1>{title}</h1>
+        <p>{text}</p>
+      </div>
+    </div>
+    <div className="hexAfter" style={{
+      display: "block",
+      paddingBottom: `calc(100% / ${Math.tan(sixtyDeg)})`
+    }}>
+    </div>
+  </div>
+)};
+
+export default Hex;
