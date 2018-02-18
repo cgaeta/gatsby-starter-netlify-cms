26b60acd06d397ebae44fe2cf630fabe02fecb0a
cgaeta <datopher64@gmail.com>
2018-02-18 17:05:00 -0500

    Update HexGridPreview.js

--- src/cms/preview-templates/HexGridPreview.js
+++ src/cms/preview-templates/HexGridPreview.js
@@ -7,9 +7,7 @@ const HexGridPreview = ({ entry }) => {
   return (
     <div>
       <pre>{JSON.stringify(test, null, 2)}</pre>
-      {test.toJS().map(hex =>
-        <pre>{JSON.stringify(hex, null, 2)}</pre>
-      )}
+      <pre>{JSON.stringify(test.toJS(), null, 2)}</pre>
     </div>
   )
 };
