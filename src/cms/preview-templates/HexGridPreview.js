9ea997d87661260540aed6a44d3b8dc563488e93
cgaeta <datopher64@gmail.com>
2018-02-18 17:19:47 -0500

    Update HexGridPreview.js

--- src/cms/preview-templates/HexGridPreview.js
+++ src/cms/preview-templates/HexGridPreview.js
@@ -3,9 +3,8 @@ import Hex from '../../components/Hex.js';
 import HexGrid from '../../components/HexGrid.js';
 
 const HexGridPreview = ({ entry }) => {
-  let test = entry.getIn(['data', 'hexes']);
   return (
-    <HexGrid hex={entry.getIn(['data', 'hexes'])} n={entry.getIn(['data', 'n'])} />
+    <HexGrid hex={entry.getIn(['data', 'hexes']).toJS()} n={entry.getIn(['data', 'n'])} />
   )
 };
 
