5836a2fd0d4d72bbb372bcfa5286e81587c9e5b9
cgaeta <datopher64@gmail.com>
2018-02-17 20:18:48 -0500

    Update Hex.js

--- src/components/Hex.js
+++ src/components/Hex.js
@@ -5,7 +5,34 @@ const sixtyDeg = Math.PI / 3;
 const Hex = (props) => {
   
   return (
-  <pre>{JSON.stringify(props)}</pre>
+  <div className="hex" style={{
+    position: "relative",
+    outline: "1px solid transparent",
+    width: `calc(100% / 2)`
+  }}>
+    <pre>{JSON.stringify(props, null, 2)}</pre>
+    <div className="hexIn" style={{
+      position: "absolute",
+      width: "96%",
+      paddingBottom: `calc(96% / ${Math.sin(sixtyDeg)})`,
+      margin: "0 2%",
+      overflow: "hidden",
+      visibility: "hidden",
+      transform: "rotate3d(0,0,1, -60deg) skewY(30deg)",
+      outline: "1px solid transparent"
+    }}>
+      <div className="hexLink">
+        <img src={src} alt />
+        <h1>{title}</h1>
+        <p>{text}</p>
+      </div>
+    </div>
+    <div className="hexAfter" style={{
+      display: "block",
+      paddingBottom: `calc(100% / ${Math.tan(sixtyDeg)})`
+    }}>
+    </div>
+  </div>
 )};
 
 export default Hex;
