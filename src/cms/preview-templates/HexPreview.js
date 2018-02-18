77129e0e1e2fca35c1d39a7f487abfc3d23236da
cgaeta <datopher64@gmail.com>
2018-02-17 19:44:20 -0500

    Update HexPreview.js

--- src/cms/preview-templates/HexPreview.js
+++ src/cms/preview-templates/HexPreview.js
@@ -2,16 +2,12 @@ import React from 'react';
 import { Hex } from '../../components/Hex.js';
 
 const HexPreview = ({ entry }) => {
-  
-  let test = {
-    src: entry.getIn(['data', 'src']),
-    text: entry.getIn(['data', 'text']),
-    title: entry.getIn(['data', 'title']),
-    n: entry.getIn(['data', 'n'])
-  };
-  
   return (
-    <pre>{JSON.stringify(test, null, 2)}</pre>
+    <Hex
+      src={entry.getIn(['data', 'src'])}
+      text={entry.getIn(['data', 'text'])}
+      title={entry.getIn(['data', 'title'])}
+      n={entry.getIn(['data', 'n'])} />
   )
 };
 
