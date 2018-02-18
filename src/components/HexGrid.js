e794c0741029013d7ba5b11823abd2170dcf6085
cgaeta <datopher64@gmail.com>
2018-02-18 15:47:58 -0500

    Create HexGrid.js

---
+++ src/components/HexGrid.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Hex from './Hex.js';
+
+const HexGrid = ({hexes, n}) => {
+  return (
+    <div className="hexGrid">
+      {hexes.map(hex =>
+        <Hex {...hex} n={n} />
+      )}
+    </div>
+  )
+}
