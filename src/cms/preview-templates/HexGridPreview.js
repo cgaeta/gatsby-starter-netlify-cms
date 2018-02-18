4edfd006e923922de5c9a937f0d79994ec8f8934
cgaeta <datopher64@gmail.com>
2018-02-18 15:52:22 -0500

    Create HexGridPreview.js

---
+++ src/cms/preview-templates/HexGridPreview.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Hex from '../../components/HexGrid.js';
+
+const HexGridPreview = ({ entry }) => {
+  let test = entry.getIn(['data', 'hexes']);
+  return (
+    <pre>{JSON.stringify(test, null, 2}</pre>
+  )
+};
+
+export default HexPreview;
