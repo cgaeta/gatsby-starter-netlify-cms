968ac6386dd39c2ebaa5c1c980dd1c7b65341509
cgaeta <datopher64@gmail.com>
2018-02-19 09:21:39 -0500

    Update HexGrid.js

--- src/components/HexGrid.js
+++ src/components/HexGrid.js
@@ -8,9 +8,9 @@ const HexGrid = ({hexes, n}) => {
       flexWrap: "wrap"
     }}>
       {hexes.map((hex, i) => {
-          console.log("building hex:", i, "indent?", (i % (n+1) === n));
+          console.log("building hex:", i, "indent?", (i % (n+1) === n-1));
           console.log("n:", n, "i % (n+1):", i % (n+1));
-          return <Hex {...hex} n={n} indent={i % (n+1) === n}/>
+          return <Hex {...hex} n={n} indent={i % (n+1) === n-1}/>
         }
       )}
     </div>
