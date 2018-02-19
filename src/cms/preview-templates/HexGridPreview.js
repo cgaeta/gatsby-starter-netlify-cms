f97804ff1efea59cca87f43f47bdc70b0eee854e
Chris Gaeta <chrisgaeta@Gaetas-iMac-7.local>
2018-02-19 16:40:03 -0500

    console log

--- src/cms/preview-templates/HexGridPreview.js
+++ src/cms/preview-templates/HexGridPreview.js
@@ -5,6 +5,7 @@ import HexGrid from '../../components/HexGrid.js';
 const HexGridPreview = ({ entry }) => {
   let hexes = entry.getIn(['data', 'hexes']);
   hexes = hexes ? hexes.toJS() : [];
+  console.log(hexes);
   return (
     <HexGrid
       hexes={hexes}
