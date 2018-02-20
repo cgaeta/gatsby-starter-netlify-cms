694d4112788eedaa7dad6188900334962f3487f4
Chris Gaeta <chrisgaeta@Gaetas-iMac-7.local>
2018-02-20 13:52:38 -0500

    testing a form

--- src/pages/index.js
+++ src/pages/index.js
@@ -27,7 +27,10 @@ export default class IndexPage extends React.Component {
         />
         <div className="container">
           <div className="content">
-            <h1 className="has-text-weight-bold is-size-2">Latest Stories</h1>
+            <form name="test" netlify>
+              <label>Name: <input type="text" name="name" /></label>
+              <input type="submit" value="Go" />
+            </form>
           </div>
         </div>
       </section>
