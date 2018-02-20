c86a3d795675fd2fe08985c0ef9faf67afc078e9
Chris Gaeta <chrisgaeta@Gaetas-iMac-7.local>
2018-02-20 13:46:02 -0500

    move HexGrid styles

--- src/components/HexGrid.js
+++ src/components/HexGrid.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import Hex from './Hex.js';
 
+import './HexGrid.scss';
+
 const longShort = (idx, len) => idx % (2 * len - 1) === len;
 const shortLong = (idx, len) => idx % (2 * len - 1) === 0;
 
 const HexGrid = ({hexes = [], count, alt}) => {
   count = parseInt(count, 10);
   return (
-    <div className="hexGrid" style={{
-      display: "flex",
-      flexWrap: "wrap"
-    }}>
+    <div className="hexGrid">
       {hexes.map((hex, i) => {
           if (!hex.count) {
             hex.count = count;
