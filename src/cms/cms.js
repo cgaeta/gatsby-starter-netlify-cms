37b1b419f3d5e586b857da135267a8009a76d1b2
Chris Gaeta <chrisgaeta@Gaetas-iMac-7.local>
2018-02-21 10:15:11 -0500

    add preview widget

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -3,8 +3,9 @@ import 'netlify-cms/dist/cms.css';
 
 import HexPreview from './preview-templates/HexPreview';
 import HexGridPreview from './preview-templates/HexGridPreview';
-
+import NavbarPreview from './preview-templates/NavBarPreview';
 
 CMS.registerPreviewStyle('/styles.css');
 CMS.registerPreviewTemplate('hex', HexPreview);
 CMS.registerPreviewTemplate('hexGrid', HexGridPreview);
+CMS.registerPreviewTemplate('navbar', NavbarPreview);
