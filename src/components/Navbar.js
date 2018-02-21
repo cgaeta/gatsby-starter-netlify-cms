9058df192cddd78e17ff6b6b99e120efc7492166
Chris Gaeta <chrisgaeta@Gaetas-iMac-7.local>
2018-02-21 10:41:36 -0500

    remove github boilerplate

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import Link from 'gatsby-link';
 
-import github from '../img/github-icon.svg';
-import logo from '../img/logo.svg';
-
 const Navbar = ({ brand, links }) => (
   <nav className="navbar is-transparent">
     <div className="container">
@@ -20,13 +17,6 @@ const Navbar = ({ brand, links }) => (
         {links.map(({text, url}) =>
           <Link to={url} className="navbar-item">{text}</Link>
         )}
-      </div>
-      <div className="navbar-end">
-        <a className="navbar-item" href="https://github.com/AustinGreen/gatsby-netlify-cms-boilerplate" target="_blank" rel="noopener noreferrer">
-          <span className="icon">
-            <img src={github} alt="Github" />
-          </span>
-        </a>
       </div>
     </div>
   </nav>
